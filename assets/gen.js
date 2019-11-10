2ea0dd0eb56ce779a5cd47d59cfc451fe8411431
twopheat <twopheat@gmail.com>
2019-11-10 02:03:36 -0800

    final commit for the night, super tired

--- assets/gen.js
+++ assets/gen.js
@@ -2,16 +2,16 @@
 var generateEl = document.querySelector("#generate");
 var copyEl = document.querySelector("#Copy");
 var specialChar = document.querySelector("#sChar")
-var keylist="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-var keyspecial="'(!)*+,-./:;<=>?@[]^_`{|}~";
+var keylist = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+var keyspecial = "'(!)*+,-./:;<=>?@[]^_`{|}~";
 
 // Function to create random password
-function makePass(){
-    var UserInput=pLength.value.replace(/[^0-9.]/g,""); //This does NOT limit to 128
+function makePass() {
+    var UserInput = pLength.value.replace(/[^0-9.]/g,"");
     // Update input value
-    pLength.value=UserInput;
+    pLength.value = UserInput;
     // Declare local variables for makePass
-    var Results=document.getElementById('results');
+    var Results = document.getElementById('results');
     var pass = "";
     // Is input empty?
     if(pLength!==''){
@@ -26,22 +26,49 @@ function makePass(){
     function passArray() {
         var foo = [];
 
-        for (var i = 0; i <= 128; i++) {//This does NOT limit to 128
+        for (var i = 0; i <= 128; i++) {//This does NOT limit to 8-128 as I had hoped
             foo.indexOf(i);
             
         };
     }
     }
+
+    function makePass() {
+    var UserInput = pLength.value.replace(passArray(),"");
+    // Update input value
+    pLength.value = UserInput;
+    // Declare local variables for makePass
+    var Results = document.getElementById('results');
+    var pass = "";
+    // Is input empty?
+    if(pLength!= 8-128){
+        for( var i=0; i < pLength.value; i++ ){
+            pass += keylist.charAt(Math.floor(Math.random() * keylist.length));
+        }
+        Results.innerHTML=pass;
+        
+    }
+    
+
+    function passArray() {
+        var foo = [];
+
+        for (var i = 8; i <= 128; i++) {//This does NOT limit to 8-128 as I had hoped
+            foo.indexOf(i);
+            };
+    }
+    }
     
 //below two functions are supposed to copy to clipboard, no dice there
 function Copy() {
-    var copyText = document.getElementById("results");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
+    document.getElementById("results").focus({preventScroll:false});
     document.execCommand("copy");
     alert("Copied");
   }
 
+focusMethod = function getFocus() {           
+   document.getElementById("results").focus();
+}
 // EventListeners for button click events
 
 copyEl.addEventListener("click", Copy.bind(this));  
